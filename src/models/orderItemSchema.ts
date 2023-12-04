d42cf09d09088db93f1fe721d4235f820d356fdc
tchisama <tchisama41@gmail.com>
2023-12-04 13:55:06 +0100

    all good about check out even custom sheets

--- src/models/orderItemSchema.ts
+++ src/models/orderItemSchema.ts
@@ -11,6 +11,28 @@ const stickerSheetSchema = new mongoose.Schema({
     }
 })
 
+
+
+const customSheetSchema = new mongoose.Schema({
+    container : { type: String, required: true},
+    data:{
+        items:{
+            type: [{
+                x: { type: Number, required: true },
+                y: { type: Number, required: true },
+                width: { type: String, required: true },
+                height: { type: String, required: true },
+                image: { type: String, required: true },
+            }],
+            required: true
+        }
+    }
+})
+
+
+
+
+
 const cupSchema = new mongoose.Schema({
     quantity: { type: Number, required: true},
     data:{
@@ -41,10 +63,13 @@ const orderItemSchema = new mongoose.Schema({
     state : { type: String, enum: ["new", "pending","printed"], default: "new", required: false},
     orderId: { type: String, required: true},
     image : { type: String, required: true},
-    type: { type: String, required: true ,enum: ["sticker-sheet", "t-shirt", "cup"]},
+    type: { type: String, required: true ,enum: ["sticker-sheet","custom-sheet", "t-shirt", "cup"]},
     tShirtSchema,
     cupSchema,
-    stickerSheetSchema
+    stickerSheetSchema,
+    customSheetSchema
+},{
+    timestamps: true
 })
 
 const OrderItem = mongoose.model("OrderItem", orderItemSchema);
