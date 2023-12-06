fd0adc0770631eafe315cac10af2b716a92f3eaa
tchisama <tchisama41@gmail.com>
2023-12-06 17:47:40 +0100

    delete and update pack and sticker sheet

--- src/routes/packRoutes.ts
+++ src/routes/packRoutes.ts
@@ -1,11 +1,23 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { createPack, getPacks } from '../controllers/packController';
+import Pack from '../models/packSchema';
+import Sticker from '../models/stickerSchema';
 
 
 const router = express.Router();
 
 router.post('/create', createPack);
 router.get('/all', getPacks)
-
+// delete pack by id and all stickers in it 
+router.delete("/:id", async (req: Request, res: Response) => {
+    try {
+        const deletedPack = await Pack.findByIdAndDelete(req.params.id);
+        // now delete all the stickers with same packId
+        await Sticker.deleteMany({ pack: req.params.id });
+        return res.status(200).json(deletedPack);
+    } catch (err) {
+        return res.status(500).json({ message: err });
+    }
+})
 
 export default router
--- src/routes/stickerSheetRoutes.ts
+++ src/routes/stickerSheetRoutes.ts
@@ -37,4 +37,23 @@ router.get("/:name", async (req: Request, res: Response) => {
 })
 
 
+// delete by id
+router.delete("/:id", async (req: Request, res: Response) => {
+    try {
+        const deletedStickerSheet = await StickerSheet.findByIdAndDelete(req.params.id);
+        return res.status(200).json(deletedStickerSheet);
+    } catch (err) {
+        return res.status(500).json({ message: err });
+    }
+})
+
+// update by id
+router.patch("/:id", async (req: Request, res: Response) => {
+    try {
+        const updatedStickerSheet = await StickerSheet.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        return res.status(200).json(updatedStickerSheet);
+    } catch (err) {
+        return res.status(500).json({ message: err });
+    }
+})
 export default router
