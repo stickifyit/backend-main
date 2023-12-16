2461c9a5d00b199250e562588ce8140f0c1c5e13
tchisama <tchisama41@gmail.com>
2023-12-16 08:56:54 +0100

    make the schema of contact

---
+++ src/models/contact.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose"
+
+const contactSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+})
+
+
+export default mongoose.model("Contact", contactSchema)
