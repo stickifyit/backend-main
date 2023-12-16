1a3a2d89f75b3cba5b94f21a9a7f6cae35b16185
tchisama <tchisama41@gmail.com>
2023-12-16 08:57:15 +0100

    make the routes of the contact

---
+++ src/routes/contactRoutes.ts
@@ -0,0 +1,38 @@
+import express , { Request, Response, Router } from 'express'
+
+import Contact from '../models/contact'
+const router: Router = express.Router();
+
+
+// make new contact message
+
+router.post('/create', async (req: Request, res: Response) => {
+    try{
+        const newContact = new Contact({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            phone: req.body.phone,
+            message: req.body.message
+        })
+        const savedContact = await newContact.save();
+        res.status(201).json(savedContact);
+    }catch(err){
+        res.status(500).json({message: err});
+        console.log(err)
+    }
+})
+
+// get all the contact messages ordered with time
+
+router.get('/all', async (req: Request, res: Response) => {
+    try{
+        const contacts = await Contact.find().sort({createdAt: -1});
+        return res.status(200).json(contacts);
+    }catch(err){
+        return res.status(500).json({message: err});
+    }
+})
+
+
+
+export default router
